f1c392a8b86405759cb1659f96b9f3b6f3af298c
Steven Paul Gray <aquasaurious@yahoo.com>
2017-05-31 17:10:22 -0600

    preparing to deploy on heroku

--- index.js
+++ index.js
@@ -7,46 +7,44 @@ var cors = require('cors');
 var massive = require('massive');
 var bodyParser = require('body-parser');
 
-var connString = "postgres://postgres:hose88@127.0.0.1:3000/gibson";
+var connString = "postgres://xejkelis:VaOlrEOnc3t0QDedLzDVho4rb5N2FgPG@stampy.db.elephantsql.com:5432/xejkelis";
 const port = 8080;
 
 var app = express();
 
 
 var guitars = require('./controllers/guitar_selector.js');
-// var corsOptions = {
-//     origin: 'https://localhost:' + port
-// };
 
-// app.use(cors(corsOptions));
 //console.log(cors.origin);
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('./public'));
 
- var db = massive.connect({connectionString : connString},
+var db = massive.connect({connectionString : connString},
    function(err, localdb){
+     console.log("massive err: ", err, ":", localdb.insert_guitar);
      db = localdb;
-      app.set('db', db);
     
      
  })
 
-/*
+
 app.get('/', function(req, res, next) {
   db.get_guitars(function(err, prod) {
     console.log("hello from your database on ", db.connectionString);
+    console.log("db get err: ", err);
     res.status(200).json(prod);
   })
 })
-*/
+
 
 app.post('/guitars', function(req, res, next) {
-  for (var i=0; i < guitars.length; i++) {
+  for (var i=20; i < 24; i++) {
     guitar = guitars[i];
     //console.log(guitar);
+    setTimeout(function(){console.log("inserting" + guitars[i].model)}, 2000);
     db.insert_guitar(guitar.classification, guitar.imgsrc, guitar.model, guitar.qualities, guitar.price, function(err, prod) {
-      res.status(200).send('bonsoir monsoiur');
+      res.status(200);
     })
   }
 })
